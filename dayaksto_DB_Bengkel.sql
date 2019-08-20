27c58476d9e98879534da6068dbceda76fef4cb0
erindafebrian <53324188+erindafebrian@users.noreply.github.com>
2019-08-20 11:41:23 +0700

    Update dayaksto_DB_Bengkel.sql

--- dayaksto_DB_Bengkel.sql
+++ dayaksto_DB_Bengkel.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dayaksto_DB_Bengkel`
+-- Database: `dbbengkel`
 --
 
 -- --------------------------------------------------------
